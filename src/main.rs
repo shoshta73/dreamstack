b77614519bba4118a16e085d04c9103b2cbcdb86
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-06-11 14:54:32 +0200

    feat: logging

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,24 @@ mod player;
 
 use game::Clock;
 use player::Player;
+use tracing::info;
+use tracing_subscriber::{
+    EnvFilter, Layer, fmt::layer, layer::SubscriberExt, registry, util::SubscriberInitExt,
+};
 
 #[tokio::main]
 async fn main() {
+    let terminal = layer()
+        .with_ansi(true)
+        .with_filter(if cfg!(debug_assertions) {
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("trace"))
+        } else {
+            EnvFilter::new("info")
+        });
+
+    registry().with(terminal).init();
+
+    info!("Game Started");
     let _player = Player::default();
     let ticker = tokio::spawn(async move {
         let mut clock = Clock::default();
@@ -17,8 +32,12 @@ async fn main() {
             interval.tick().await;
             clock.tick();
             println!("game time {clock}");
+            if clock.minute() == 1 {
+                break;
+            }
         }
     });
 
     ticker.await.expect("ticker task failed");
+    info!("Game Ended");
 }
